215bc1e999f1c659f47e5385dc39211fb6ff6385
Nikki Campos <41343897+nikkiwizard@users.noreply.github.com>
2019-01-17 17:46:25 -0500

    Add "Mad Libs" game
    Not very good since it's one story but it's based on a true story. have fun ;)

---
+++ Mad Libs.py
@@ -0,0 +1,34 @@
+#inputs
+country = input("Please choose a place: ")
+high_school = input("Please choose a noun: ")
+rowdy = input("Please choose an adjective: ")
+two = int(input("Please choose an integer: "))
+excited = input("Please choose an adjective: ")
+cute = input("Please choose an adjective: ")
+male = input("Please choose a noun: ")
+smiling = input("Please choose a verb ending in -ing: ")
+cute2 = input("Please choose an adjective: ")
+great = input("Please choose an adjective: ")
+cried = input("Please choose a verb ending in -ed: ")
+
+#actual story
+print("")
+print(f"Once upon a time, there was a young girl in an hispanic {country}. Her mom was going to her {high_school} reunion.") 
+print(f"She tagged along to said party and met all her mom's {rowdy} friends.")
+print(input("Hit enter to continue... "))
+print(str.swapcase("tHE MUSIC STARTED OUT GOOD BUT SOON TURNED boring."))
+print("By ", two, " AM, she wanted to go home. But then! The mariachis showed up for a serenade.")
+print("She was so ", excited, " to see her first mariachi band in person. She started taking videos, and then she saw him.")
+print(input("Hit enter to continue... "))
+print(f"Surprisingly young, the lead singer was a {cute} {male}. She tried to take a photo of him in secret admiration, but he caught her.") 
+print(f"He started {smiling} at her, and she blushed. He pulled her out to dance while he sang, and the girl couldn't believe it.") 
+print(f"A song later, everyone got up to take photos and videos with the {cute2} mariachi boy.")
+print(input("Hit enter to continue... "))
+print(str.title("The girl managed to stand right next to him. He put his arm around her, and she died inside."))
+print("Right before the mariachis were set to leave, the girl went up to the guy and told him he was a ", great, " singer.") 
+print("He smiled and leaned down to whisper,'You know, you have a cute smile.'")
+print("He asked for her phone number, and then went home and ", cried, " because he was so beautiful.")
+
+#Once upon a time, there was a young girl in an hispanic [country]. her mom was going to her [high school] reunion. she tagged along to said party and met all her mom's [rowdy] friends. the music started out good but soon turned boring. By [2] am, she wanted to go home. and then ! the mariachis showed up for a serenade. she was so [excited] to see her first mariachi band in person. she started taking videos, and then she saw him. surprisingly young, the lead singer was a [cute] [male]. she tried to take a photo of him in secret admiration, but he caught her. he started [smiling] at her and she blushed. he pulled her out to dance while he sang, and the girl couldnt believe it. a song later, everyone got up to take photos and videos with the [cute] mariachi boy, and the girl managed to stand right next to him. He put his arm around her and she died inside. 
+#Right before the mariachis were set to leave, the girl went up to the guy and told him he was a [great] singer. he smiled and leaned down to whisper, "you know, you have a cute smile"
+#he asked for her phone number, and the girl went home and [cried] because he was so beautiful."""
